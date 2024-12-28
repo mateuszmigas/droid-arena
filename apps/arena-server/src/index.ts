3b307c3d609c6c35b1068783fc905cbd11e894aa
Mateusz Migas <54471371+mateuszmigas@users.noreply.github.com>
2024-12-28 22:23:43 +0100

    simple web sockets

--- apps/arena-server/src/index.ts
+++ apps/arena-server/src/index.ts
@@ -1,6 +1,12 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Arena Server").listen(3002);
+const app = new Elysia()
+  .ws("/ping", {
+    message(ws, message) {
+      ws.send(`Elysia says: ${message}`);
+    },
+  })
+  .listen(3002);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,15 @@
 import { add } from "@droid-arena/utils";
+import { useEffect } from "react";
 
 export const App = () => {
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:3002/ping");
+    ws.onmessage = (event) => {
+      console.log(event.data);
+    };
+    ws.onopen = () => {
+      ws.send("Hello from the client!");
+    };
+  }, []);
   return <div className="bg-red-500">Hello World {add(1, 3)}</div>;
 };
