59ba47eb604b7ad29e5ba6a2110d8cd908c7df8f
Mateusz Migas <54471371+mateuszmigas@users.noreply.github.com>
2024-12-28 21:38:57 +0100

    switch to bun

--- apps/web/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { tanksGame } from "@droid-arena/tanks";
-import { volleyGame } from "@droid-arena/volley";
-
-export default function Home() {
-  tanksGame();
-  volleyGame();
-  return <div className="size-full bg-blue-500">Select game</div>;
-}
-
---
+++ apps/web/src/App.tsx
@@ -0,0 +1,6 @@
+import { add } from "@droid-arena/utils";
+
+export const App = () => {
+  return <div>Hello World {add(1, 2)}</div>;
+};
+
